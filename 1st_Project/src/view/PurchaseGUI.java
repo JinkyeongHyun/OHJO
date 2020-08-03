520ba2554c3675d9e1f101f633bbc576dd3e98df
SMT063 <SMT063@118.40.119.132>
2020-08-03 12:25:01 +0900

    구매창 수정

--- 1st_Project/src/view/PurchaseGUI.java
+++ 1st_Project/src/view/PurchaseGUI.java
@@ -102,23 +102,23 @@ public class PurchaseGUI {
 		label_p_image.setIcon(new ImageIcon(PurchaseGUI.class.getResource("/images/"+model().get(0).getP_img())));
 		label_p_image.setForeground(Color.BLACK);
 		label_p_image.setBackground(Color.BLACK);
-		label_p_image.setBounds(12, 15, 217, 188);
+		label_p_image.setBounds(12, 10, 387, 241);
 		frame.getContentPane().add(label_p_image);
 
 		JLabel lblNewLabel = new JLabel("\uC81C\uD488\uBA85");
-		lblNewLabel.setBounds(258, 47, 63, 22);
+		lblNewLabel.setBounds(451, 55, 63, 22);
 		frame.getContentPane().add(lblNewLabel);
 
 		JLabel label = new JLabel("\uC8FC\uBB38\uC77C\uC790");
-		label.setBounds(258, 79, 63, 22);
+		label.setBounds(451, 87, 63, 22);
 		frame.getContentPane().add(label);
 
 		JLabel label_1 = new JLabel("\uC8FC\uBB38\uC218\uB7C9");
-		label_1.setBounds(258, 111, 63, 22);
+		label_1.setBounds(451, 119, 63, 22);
 		frame.getContentPane().add(label_1);
 
 		JLabel label_2 = new JLabel("\uACB0\uC81C \uAE08\uC561");
-		label_2.setBounds(258, 147, 63, 22);
+		label_2.setBounds(451, 155, 63, 22);
 		frame.getContentPane().add(label_2);
 
 		JLabel label_3 = new JLabel("\uACB0\uC81C \uBC29\uBC95");
@@ -161,29 +161,29 @@ public class PurchaseGUI {
 		card.add(comboBox_month);
 
 		tf_name = new JTextField();
-		tf_name.setBounds(143, 246, 116, 21);
+		tf_name.setBounds(143, 312, 116, 21);
 		frame.getContentPane().add(tf_name);
 		tf_name.setColumns(10);
 
 		tf_address = new JTextField();
-		tf_address.setBounds(143, 340, 383, 21);
+		tf_address.setBounds(143, 406, 383, 21);
 		frame.getContentPane().add(tf_address);
 		tf_address.setColumns(10);
 
 		tf_cell = new JTextField();
-		tf_cell.setBounds(143, 277, 215, 21);
+		tf_cell.setBounds(143, 343, 215, 21);
 		frame.getContentPane().add(tf_cell);
 		tf_cell.setColumns(10);
 
 		JLabel lb_name = new JLabel(model().get(0).getP_name());
-		lb_name.setBounds(332, 47, 287, 22);
+		lb_name.setBounds(525, 55, 121, 22);
 		frame.getContentPane().add(lb_name);
 		
 		String price = Integer.toString((model().get(0).getP_price()) );
 		int price1 = model().get(0).getP_price();
 		
 		JLabel lb_price = new JLabel(price);
-		lb_price.setBounds(333, 147, 287, 22);
+		lb_price.setBounds(526, 155, 120, 22);
 		frame.getContentPane().add(lb_price);
 		
 		
@@ -197,7 +197,7 @@ public class PurchaseGUI {
 		
 		
 		tf_amount.setText("1");
-		tf_amount.setBounds(333, 112, 63, 21);
+		tf_amount.setBounds(526, 120, 63, 21);
 		frame.getContentPane().add(tf_amount);
 		tf_amount.setColumns(10);
 		int amount = Integer.parseInt(tf_amount.getText());
@@ -280,52 +280,42 @@ public class PurchaseGUI {
 
 		JLabel label_4 = new JLabel("\uBC30\uC1A1\uC9C0");
 		label_4.setFont(new Font("±¼¸²", Font.PLAIN, 18));
-		label_4.setBounds(12, 213, 63, 22);
+		label_4.setBounds(12, 279, 63, 22);
 		frame.getContentPane().add(label_4);
 
 		JLabel label_5 = new JLabel("\uC774\uB984");
-		label_5.setBounds(80, 245, 34, 22);
+		label_5.setBounds(80, 311, 34, 22);
 		frame.getContentPane().add(label_5);
 
 		JLabel label_6 = new JLabel("\uC5F0\uB77D\uCC98");
-		label_6.setBounds(78, 276, 53, 22);
+		label_6.setBounds(78, 342, 53, 22);
 		frame.getContentPane().add(label_6);
 
 		JLabel label_7 = new JLabel("\uC8FC\uC18C");
-		label_7.setBounds(80, 308, 34, 22);
+		label_7.setBounds(80, 374, 34, 22);
 		frame.getContentPane().add(label_7);
-
-		JLabel label_8 = new JLabel("\uBC30\uC1A1\uC2DC \uC694\uCCAD\uC0AC\uD56D");
-		label_8.setBounds(40, 370, 103, 22);
-		frame.getContentPane().add(label_8);
 
 		JLabel label_9 = new JLabel("\uAD6C\uB9E4\uC815\uBCF4");
 		label_9.setFont(new Font("±¼¸²", Font.PLAIN, 19));
-		label_9.setBounds(241, 15, 80, 22);
+		label_9.setBounds(434, 23, 80, 22);
 		frame.getContentPane().add(label_9);
 
-		JComboBox cmb_request = new JComboBox();
-		cmb_request.setModel(
-				new DefaultComboBoxModel(new String[] { "\uBC30\uC1A1\uC2DC \uC694\uCCAD\uC0AC\uD56D \uC120\uD0DD",
-						"\uBD80\uC7AC\uC2DC\uC5D0 \uACBD\uBE44\uC2E4\uC5D0 \uB9E1\uACA8\uC8FC\uC138\uC694.",
-						"\uBB38\uC55E\uC5D0 \uB194\uB46C\uC8FC\uC138\uC694.",
-						"\uBD80\uC7AC\uC2DC \uC5F0\uB77D\uCC98\uB85C \uC5F0\uB77D\uC8FC\uC138\uC694.",
-						"\uD0DD\uBC30\uD568\uC5D0 \uB123\uC5B4 \uC8FC\uC138\uC694." }));
-		cmb_request.setBounds(143, 371, 207, 21);
-		frame.getContentPane().add(cmb_request);
-
 		JLabel lblNewLabel_2 = new JLabel("\uC6B0\uD3B8\uBC88\uD638");
-		lblNewLabel_2.setBounds(143, 312, 63, 15);
+		lblNewLabel_2.setBounds(143, 378, 63, 15);
 		frame.getContentPane().add(lblNewLabel_2);
 
 		tf_Postalcode = new JTextField();
-		tf_Postalcode.setBounds(205, 309, 116, 21);
+		tf_Postalcode.setBounds(205, 375, 116, 21);
 		frame.getContentPane().add(tf_Postalcode);
 		tf_Postalcode.setColumns(10);
 
 		JLabel label_12 = new JLabel("");
 		label_12.setBounds(333, 79, 143, 22);
 		frame.getContentPane().add(label_12);
+		
+		JLabel lbl_date = new JLabel(dao.numsearch().get(0).getDate());
+		lbl_date.setBounds(526, 87, 120, 22);
+		frame.getContentPane().add(lbl_date);
 		
 	}
 }
